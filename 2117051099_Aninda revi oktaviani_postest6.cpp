fb3f9a45837da1193d59f28a8acf50d380384c5f
anindarevioktaviani <92665686+anindarevioktaviani@users.noreply.github.com>
2022-01-05 15:27:22 +0700

    Add files via upload

---
+++ 2117051099_Aninda revi oktaviani_postest6.cpp
@@ -0,0 +1,121 @@
+#include <ncurses/ncurses.h>
+#include <windows.h>
+#include <unistd.h>
+using namespace std;
+
+int main(){
+	initscr();
+	start_color();
+	init_pair(1,COLOR_BLUE,COLOR_WHITE);
+	attron (COLOR_PAIR(1));
+	
+	printw ("Aninda revi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aNinda revi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("anInda revi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	  	
+			
+	printw ("aniNda revi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+		
+	
+	printw ("aninDa revi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("anindA revi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+		
+    printw ("aninda Revi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda rEvi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda reVi oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revI oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi Oktaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi oKtaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi okTaviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi oktAviani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi oktaViani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi oktavIani");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi oktaviAni");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi oktaviaNi");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	printw ("aninda revi oktavianI");
+	refresh();
+	Sleep(1000);
+	clear();
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+
+
+
